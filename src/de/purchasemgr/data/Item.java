b7c1a5293c5aedb082e0a6dc415bc455d954dbb9
croesch <croesch@192.168.0.101>
2011-01-14 02:36:06 +0100

    revision

--- src/de/purchasemgr/data/Item.java
+++ src/de/purchasemgr/data/Item.java
@@ -6,7 +6,7 @@ import de.purchasemgr.i18n.Messages;
  * This is the representation for an item, the clone from the reality.
  * 
  * @author $Author: croesch $
- * @version $Revision: 1.2 $ ($Date: 2010/12/19 00:03:13 $)
+ * @version ($Date: 2010/12/19 00:03:13 $)
  */
 class Item {
 
