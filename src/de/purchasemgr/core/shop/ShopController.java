29fd03c623f45754cbfc501d464a6b4bf7ebc0a2
croesch <croesch@users.sourceforge.net>
2011-02-06 20:09:13 +0100

    removed checkstyle warnings

--- src/de/purchasemgr/core/shop/ShopController.java
+++ src/de/purchasemgr/core/shop/ShopController.java
@@ -7,17 +7,21 @@ import de.purchasemgr.i18n.Messages;
 import de.purchasemgr.logging.LogManager;
 
 /**
- * This is the shop controller, it controls all things that have to do with {@link Shop}s
+ * This is the shop controller, it controls all things that have to do with
+ * {@link Shop}s
  * 
  * @author croesch
  * @since Date: 16.01.2011 12:47:28
  */
 public class ShopController {
 
+  /** the model of this controller */
   private final ShopModel model = new ShopModel();
 
+  /** the view of this controller */
   private final ShopView view = new ShopView(this);
 
+  /** the index of the shop to edit */
   private int editIndex = 0;
 
   /**
@@ -27,7 +31,7 @@ public class ShopController {
    * @since Date: 16.01.2011 12:51:13
    * @return a {@link JComboBox} that contains all {@link Shop}s available
    */
-  public JComboBox getShopBox() {
+  public final JComboBox getShopBox() {
     return this.view.getBox(this.model.getShops());
   }
 
@@ -38,20 +42,38 @@ public class ShopController {
    * @since Date: 16.01.2011 16:07:38
    * @return the number of shops that are available
    */
-  public int getShopCount() {
+  public final int getShopCount() {
     return this.model.getShopCount();
   }
 
-  void createShop(String name, String postCode, String location) {
-    this.model.addShop(new Shop(name, postCode, location));
+  /**
+   * Performs the storage of a new shop
+   * 
+   * @author croesch
+   * @since Date: 06.02.2011 19:56:34
+   * @param n the name of the new shop
+   * @param pc the post code of the new shop
+   * @param loc the location of the new shop
+   */
+  final void createShop(final String n, final String pc, final String loc) {
+    this.model.addShop(new Shop(n, pc, loc));
     this.view.updateBox(this.model.getShops());
   }
 
-  void save(String name, String postCode, String location) {
+  /**
+   * Performs the update of the edited shop
+   * 
+   * @author croesch
+   * @since Date: 06.02.2011 19:57:37
+   * @param n the new name for the shop
+   * @param pc the new post code for the shop
+   * @param loc the new location for the shop
+   */
+  final void save(final String n, final String pc, final String loc) {
     Shop edited = this.model.get(this.editIndex);
-    edited.setName(name);
-    edited.setPostCode(postCode);
-    edited.setLocation(location);
+    edited.setName(n);
+    edited.setPostCode(pc);
+    edited.setLocation(loc);
     this.model.set(this.editIndex, edited);
   }
 
@@ -61,7 +83,7 @@ public class ShopController {
    * @author croesch
    * @since Date: 16.01.2011 16:08:25
    */
-  public void newShop() {
+  public final void newShop() {
     this.view.newShop();
   }
 
@@ -71,7 +93,7 @@ public class ShopController {
    * @author croesch
    * @since Date: 16.01.2011 16:08:58
    */
-  public void editShops() {
+  public final void editShops() {
     if (this.editIndex >= 0) {
       this.view.editShop(this.editIndex + 1, this.model.get(this.editIndex));
     } else {
@@ -79,12 +101,26 @@ public class ShopController {
     }
   }
 
-  void decrementEditingShop() {
+  /**
+   * Changes the current editing shop to the one before the current one
+   * 
+   * @author croesch
+   * @since Date: 06.02.2011 19:59:50
+   * @see #incrementEditingShop()
+   */
+  final void decrementEditingShop() {
     --this.editIndex;
     editShops();
   }
 
-  void incrementEditingShop() {
+  /**
+   * Changes the current editing shop to the one after the current one
+   * 
+   * @author croesch
+   * @since Date: 06.02.2011 20:00:37
+   * @see #decrementEditingShop()
+   */
+  final void incrementEditingShop() {
     ++this.editIndex;
     editShops();
   }
@@ -96,7 +132,7 @@ public class ShopController {
    * @since Date: 23.01.2011 14:56:20
    * @return the selected shop of the shop-box
    */
-  public Shop getSelectedShop() {
+  public final Shop getSelectedShop() {
     return this.model.get(this.view.getSelectedIndex());
   }
 }
--- src/de/purchasemgr/core/shop/ShopModel.java
+++ src/de/purchasemgr/core/shop/ShopModel.java
@@ -13,14 +13,15 @@ import de.purchasemgr.data.type.Shop;
  */
 class ShopModel {
 
+  /** the list that contains the stored shops */
   private final List<Shop> shops = new ArrayList<Shop>();
 
   /**
    * adds the given shop to the list
    * 
-   * @param p the {@link Shop} to add
+   * @param s the {@link Shop} to add
    */
-  void addShop(Shop s) {
+  void addShop(final Shop s) {
     this.shops.add(s);
   }
 
@@ -31,22 +32,60 @@ class ShopModel {
     return this.shops;
   }
 
-  void remove(int i) {
+  /**
+   * Removes the shop at the given position
+   * 
+   * @author croesch
+   * @since Date: 06.02.2011 20:02:33
+   * @see List#remove(int)
+   * @param i the index of the shop to remove
+   * @throws ArrayIndexOutOfBoundsException if the position is invalid
+   */
+  void remove(final int i) throws ArrayIndexOutOfBoundsException {
     this.shops.remove(i);
   }
 
-  Shop get(int i) {
+  /**
+   * Returns the shop at the given position
+   * 
+   * @author croesch
+   * @since Date: 06.02.2011 20:03:12
+   * @see List#get(int)
+   * @param i the index of the shop to fetch
+   * @return the {@link Shop} at the given position
+   * @throws ArrayIndexOutOfBoundsException if the position is invalid
+   */
+  Shop get(final int i) throws ArrayIndexOutOfBoundsException {
     if (i >= 0) {
       return this.shops.get(i);
     }
-    return null;
+    throw new ArrayIndexOutOfBoundsException(i);
   }
 
+  /**
+   * Returns the number of stored {@link Shop}s
+   * 
+   * @author croesch
+   * @since Date: 06.02.2011 20:03:54
+   * @see List#size()
+   * @return the size of the list of shops
+   */
   int getShopCount() {
     return this.shops.size();
   }
 
-  void set(int index, Shop shop) {
+  /**
+   * Sets the given {@link Shop} at the given position
+   * 
+   * @author croesch
+   * @since Date: 06.02.2011 20:06:18
+   * @see List#set(int, Object)
+   * @param index the index where to store the shop
+   * @param shop the new shop for the given position
+   * @throws ArrayIndexOutOfBoundsException if the given position is invalid
+   */
+  void set(final int index, final Shop shop)
+                                            throws ArrayIndexOutOfBoundsException {
     this.shops.set(index, shop);
   }
 }
